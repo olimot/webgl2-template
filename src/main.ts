252b73b31bf4d6fd7e434c20ad5b711d5c3075e4
olimot <olimot@gmail.com>
2024-10-12 21:20:59 +0900

    add comments

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { up } from "./config";
 import { listenInputEvents } from "./input";
 import { moveXY, pinchOrbit, rotateOrbit } from "./orbital";
 
+// # create cube vertices
 const faceIndicesMap = [
   [3, 7, 5, 1],
   [6, 2, 0, 4],
@@ -54,15 +55,28 @@ for (let f = 0; f < 6; ++f) {
   });
 }
 
+// # create camera matrices
 const projection = mat4.create();
 const target = vec3.fromValues(0, 0, 0);
-const view = mat4.create();
+const view = mat4.lookAt(mat4.create(), [2, 2, 2], target, up);
 const viewProjection = mat4.identity(mat4.create());
 
+// # setup canvas
 const canvas = document.getElementById("screen") as HTMLCanvasElement;
 
-mat4.lookAt(view, [2, 2, 2], target, up);
+// ## setup resize handler
+let resizeTask: number = 0;
+new ResizeObserver(([entry]) => {
+  const width = entry.devicePixelContentBoxSize[0].inlineSize;
+  const height = entry.devicePixelContentBoxSize[0].blockSize;
+  clearTimeout(resizeTask);
+  resizeTask = setTimeout(() => {
+    canvas.width = width;
+    canvas.height = height;
+  }, 150);
+}).observe(canvas, { box: "content-box" });
 
+// ## add camera control using mouse input from canvas
 listenInputEvents(canvas, ({ keys, delta, buttons }) => {
   if ((keys.Space && keys.ShiftLeft) || buttons === 5) {
     rotateOrbit(view, target, delta);
@@ -75,6 +89,7 @@ listenInputEvents(canvas, ({ keys, delta, buttons }) => {
   }
 });
 
+// # initialize webgl2 rendering context
 const gl = canvas.getContext("webgl2") as WebGL2RenderingContext;
 gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, false);
 gl.pixelStorei(gl.UNPACK_PREMULTIPLY_ALPHA_WEBGL, false);
@@ -92,7 +107,7 @@ gl.colorMask(true, true, true, true);
 gl.clearColor(31 / 255, 31 / 255, 31 / 255, 1);
 gl.clearDepth(1);
 
-// prepare VertexArray
+// # prepare vertex array
 const vertexArray = gl.createVertexArray();
 gl.bindVertexArray(vertexArray);
 gl.bindBuffer(gl.ARRAY_BUFFER, gl.createBuffer());
@@ -107,7 +122,7 @@ gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, gl.createBuffer());
 gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, indices, gl.STATIC_DRAW);
 gl.bindVertexArray(null);
 
-// prepare texture
+// # prepare texture
 const empty = new ImageData(1, 1);
 empty.data.set([255, 255, 255, 255]);
 const texture = gl.createTexture();
@@ -119,6 +134,7 @@ gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.REPEAT);
 gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.REPEAT);
 gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, empty);
 
+// # create program
 const vert = gl.createShader(gl.VERTEX_SHADER) as WebGLShader;
 const frag = gl.createShader(gl.FRAGMENT_SHADER) as WebGLShader;
 gl.shaderSource(
@@ -169,40 +185,41 @@ gl.linkProgram(program);
 console.log(gl.getShaderInfoLog(vert));
 console.log(gl.getShaderInfoLog(frag));
 
+// # get uniform locations
 gl.useProgram(program);
 const baseColorTextureLoc = gl.getUniformLocation(program, "baseColorTexture");
 const viewProjectionLoc = gl.getUniformLocation(program, "viewProjection");
 
-let resizeTask: number = 0;
-const observer = new ResizeObserver(([entry]) => {
-  const width = entry.devicePixelContentBoxSize[0].inlineSize;
-  const height = entry.devicePixelContentBoxSize[0].blockSize;
-  clearTimeout(resizeTask);
-  resizeTask = setTimeout(() => {
-    canvas.width = width;
-    canvas.height = height;
-  }, 150);
-});
-observer.observe(canvas, { box: "content-box" });
-
+// # for each frame
 requestAnimationFrame(function frame() {
+  // ## update camera materices
   const aspectRatio = canvas.clientWidth / canvas.clientHeight;
   mat4.ortho(projection, -2 * aspectRatio, 2 * aspectRatio, -2, 2, 0, 4); // alternatively
   mat4.perspective(projection, Math.PI / 4, aspectRatio, 0.01, +Infinity);
   mat4.multiply(viewProjection, projection, view);
 
+  // ## clear screen
   gl.viewport(0, 0, gl.drawingBufferWidth, gl.drawingBufferHeight);
   gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
+  // ## draw
   gl.useProgram(program);
+
+  // ### set textures
   gl.activeTexture(gl.TEXTURE0);
   gl.bindTexture(gl.TEXTURE_2D, texture);
+
+  // ### set uniforms
   gl.uniform1i(baseColorTextureLoc, texUnit);
   gl.uniformMatrix4fv(viewProjectionLoc, false, viewProjection);
+
+  // ### set global state
   gl.frontFace(gl.CCW);
   gl.enable(gl.CULL_FACE);
   gl.enable(gl.DEPTH_TEST);
   gl.disable(gl.BLEND);
+
+  // ### bind vertex array and draw
   gl.bindVertexArray(vertexArray);
   gl.drawElements(gl.TRIANGLES, indices.length, gl.UNSIGNED_BYTE, 0);
   gl.bindVertexArray(null);
